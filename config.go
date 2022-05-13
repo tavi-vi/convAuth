83fb8f00c7aefe1a1225aee4bc523127b52f27e0
Tavi <syncopegirl@gmail.com>
2022-05-13 07:11:49 -0400

    we get null pointer if we don't load the ini

--- config.go
+++ config.go
@@ -15,14 +15,16 @@ func updateConfig(args []string) error {
 	fset.StringVar(&serverConfig.cookieDomain, "cookieDomain", "", "The domain that the cookie is limited to")
 	fset.StringVar(&serverConfig.listenAddress, "listenAddress", ":8080", "Address the server listens on")
 
-	cfg, _ := ini.Load(authProxyConfig)
-	k := cfg.Section("").Key("listenAddress")
-	if k != nil {
-		serverConfig.listenAddress = k.String()
-	}
-	k = cfg.Section("").Key("cookieDomain")
-	if k != nil {
-		serverConfig.cookieDomain = k.String()
+	cfg, err := ini.Load(authProxyConfig)
+	if err == nil {
+		k := cfg.Section("").Key("listenAddress")
+		if k != nil {
+			serverConfig.listenAddress = k.String()
+		}
+		k = cfg.Section("").Key("cookieDomain")
+		if k != nil {
+			serverConfig.cookieDomain = k.String()
+		}
 	}
 
 	return fset.Parse(args)
